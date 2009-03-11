1a2cc7b5a0483f7dee76e6e5fa7a0ea010408687
Lydie Souhait <lydie_souhait@hms.harvard.edu>
2009-03-11 21:21:18 +0000

    Splashscreen added
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@934 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

--- trunk/gofigure.cxx
+++ trunk/gofigure.cxx
@@ -1,16 +1,46 @@
 #include <qapplication.h>
 #include "QGoMainWindow.h"
+#include <qsplashscreen.h>
 
 int main( int argc, char** argv )
 {
   QApplication app(argc, argv);
-  QGoMainWindow form;
+  
+  QPixmap pixmap(":/fig/splash.jpg");
+  QSplashScreen *splash = new QSplashScreen(pixmap,Qt::WindowStaysOnTopHint);
 
+  splash->show();
+ 
+  app.processEvents();
+  splash->showMessage("Application loading...please wait");
+  //splash.showMessage("Ready");
+  
+
+  QGoMainWindow form;
   if( argc == 2 )
     form.SetFileName( argv[1] );
-
+ 
   app.setMainWidget( &form );
+  
   form.show( );
-
+    
+  splash->showMessage("Application ready");
+ // app.processEvents();
+  splash->finish(&form);
+  delete splash;
   return app.exec();
 }
+
+
+
+
+
+
+
+
+
+/*establishConnections();
+mainWin.show();
+splash->finish(&mainWin);
+delete splash;
+return app.exec();*/
