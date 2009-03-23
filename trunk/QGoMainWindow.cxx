23fc6b927e7e4f975cbd9cf368b038c8129d705d
Alexandre Gouaillard <agouaillard@gmail.com>
2009-03-23 18:16:53 +0000

    ENH: move external code to the correct folder. It helps tracking external code per source, and their licence.
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@1065 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

--- trunk/QGoMainWindow.cxx
+++ trunk/QGoMainWindow.cxx
@@ -1,3 +1,37 @@
+/*=========================================================================
+ Authors: The GoFigure Dev. Team.
+ at Megason Lab, Systems biology, Harvard Medical school, 2009 
+ 
+ Copyright (c) 2009, President and Fellows of Harvard College.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ Redistributions of source code must retain the above copyright notice, 
+ this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation 
+ and/or other materials provided with the distribution.
+ Neither the name of the  President and Fellows of Harvard College 
+ nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written 
+ permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ =========================================================================*/
+
 #include "QGoMainWindow.h"
 
 #include <iostream>
@@ -517,18 +551,19 @@ void QGoMainWindow::DisplayInTab( vtkImageData* myImage, int TabIndex )
 // *****************************************************************************
 void QGoMainWindow::on_actionAbout_activated( )
 {
-  QString version( tr( "v2.0" ) );
+  QString version( tr( "v0.5" ) );
 
   QString about_gofigure( tr( "GoFigure V2 \n\n" ) );
 
   QString authors( tr( "Authors in alphabetical order:\n" ) );
   authors.append( " * 2008 ~ Arnaud Gelas\n" );
   authors.append( " * 2007 ~ Alexandre Gouaillard\n" );
-  authors.append( " * 2002 ~ Sean Megason\n" );
   authors.append( " * 2008 ~ Kishore Mosaliganti\n" );
-  authors.append( " * 2008 ~ Lydie Souhait\n" );
+  authors.append( " * 2008 ~ Lydie Souhait\n\n" );
+  authors.append( " * Principal Investigator\n" );
+  authors.append( " * Sean Megason\n" );
 
-  QString message = QString( "GoFigure %1\n\n" ).arg( version );
+  QString message = QString( "GoFigure V2 %1\n\n" ).arg( version );
   message.append( about_gofigure );
   message.append( authors );
 
