72be7d007003f3d15941eaef5a60c221fecb88a0
Alexandre Gouaillard <agouaillard@gmail.com>
2009-05-03 03:29:16 +0000

    ENH: megacapture and lsmseries supported at the QImagePageView4DTracer level.

--- Examples/GUI/QImagePageView4DTracer/ImagePageView4DTracer.cxx
+++ Examples/GUI/QImagePageView4DTracer/ImagePageView4DTracer.cxx
@@ -4,14 +4,14 @@
 
 int main( int argc, char** argv )
 {
-  if( argc < 3 ) return EXIT_FAILURE;
+  if( argc < 2 ) return EXIT_FAILURE;
 
   QApplication app(argc, argv);
   QImagePageView4DTracer form;
   form.setMinimumSize( 800, 600 );
   form.SetColorVizu( 0 );
-  form.SetFileTypeToSerie( );
-  form.SetSerieTypeToMegaCapture();
+  //form.SetFileTypeToSerie( );
+  //form.SetSerieTypeToMegaCapture();
   // filename must come last
   form.SetFileName( argv[1] );
   app.setMainWidget( &form );
