8869bcfcd4259ca0dd94a16ae6ad64624c7020a3
Alexandre Gouaillard <agouaillard@gmail.com>
2009-05-06 01:30:05 +0000

    ENH: getting ready for saving in the database.

--- Code/IO/GoDBRecordSet.h
+++ Code/IO/GoDBRecordSet.h
@@ -2,6 +2,8 @@
 #include <utility>
 #include <vector>
 
+#include "CreateDataBaseHelper.h"
+
 template< class TObject >
 class GoDBRecordSet
 {
@@ -48,13 +50,53 @@ public:
     { this->PassWord = Password; };
 
   // read content from DB
-  void PopulateFromDB() { };
+  void PopulateFromDB()
+    { 
+    if( !CanConnectToDatabase(
+        this->ServerName,
+        this->User,
+        this->PassWord,
+        this->DataBaseName
+        ) ) 
+      {
+      // throw exception
+      return;
+      }
+    };
 
   // save content to DB - ASYNCHRONOUS
   void SaveInDB()
     {
     std::sort( m_RowContainer.begin(), m_RowContainer.end(), IsLess() );
-    };
+    if( !CanConnectToDatabase(
+        this->ServerName,
+        this->User,
+        this->PassWord,
+        this->DataBaseName
+        ) ) 
+      {
+      // throw exception
+      return;
+      }
+    std::string Query = this->SetUpInsertQueryString();
+    vtkMySQLDatabase * DataBaseConnector = vtkMySQLDatabase::New();
+    DataBaseConnector->SetHostName(this->ServerName);
+    DataBaseConnector->SetUser(this->User);
+    DataBaseConnector->SetPassword(this->PassWord);
+    DataBaseConnector->SetDatabaseName( this->DataBaseName );
+    DataBaseConnector->Open();
+
+    vtkSQLQuery* query = DataBaseConnector->GetQueryInstance();
+    query->SetQuery( Query.c_str());
+    if ( !query->Execute() )
+      {
+      // replace by exception
+      std::cerr << "Create query failed" << std::endl;
+      }
+    DataBaseConnector->Close();
+    DataBaseConnector->Delete();
+    query->Delete();
+  };
 
 private:
   // functor to sort our RowContainer and optimize SQL requests
@@ -67,6 +109,12 @@ private:
       if( A.first || !B.first ) return true;
       return false;
       };
+    };
+
+  std::string SetUpInsertQueryString()
+    {
+    std::string whatever( "whatever" );
+    return whatever;
     };
 
   // underlying container
