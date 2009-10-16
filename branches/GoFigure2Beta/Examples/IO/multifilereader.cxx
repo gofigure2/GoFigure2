004479d374f0edd9383b4fdc831a680f5808d4de
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2009-10-16 15:17:47 +0000

    add multifilereader test
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@1602 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

--- branches/GoFigure2Beta/Examples/IO/multifilereader.cxx
+++ branches/GoFigure2Beta/Examples/IO/multifilereader.cxx
@@ -1,6 +1,26 @@
+#include "itkMegaCaptureImport.h"
+#include "itkMultiFileReader.h"
+
+#include "vtkImageData.h"
+
 int main( int argc, char** argv )
 {
+  itk::MegaCaptureImport::Pointer importer = itk::MegaCaptureImport::New();
+  importer->SetFileName( argv[1] );
+  importer->Update();
+  FileListType* listoffiles = importer->GetOutput();
+
   itk::MultiFileReader::Pointer reader = itk::MultiFileReader::New();
+  reader->SetInput( listoffiles );
+  reader->SetDimensionality( 2 );
+  reader->SetFileType( itk::MultiFileReader::JPEG );
+  reader->MultiChannelImagesOn();
+  reader->SetTimeBased( true );
+  reader->SetTimePoint( 0 );
+  reader->Update();
+
+  vtkImageData* image = reader->GetOutput();
+  std::cout <<*image <<std::endl;
 
   return EXIT_SUCCESS;
 }
