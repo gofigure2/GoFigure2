771b2e6db37c46d73b5426bc9503f3e5d7b33542
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2009-10-16 20:33:38 +0000

    add todo file to fix priorities for next weeks
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@1605 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

--- branches/GoFigure2Beta/Examples/GUI/lib/qgotabimageview4d.cxx
+++ branches/GoFigure2Beta/Examples/GUI/lib/qgotabimageview4d.cxx
@@ -7,6 +7,14 @@
 
 int main( int argc, char** argv )
 {
+  if( argc != 3 )
+    {
+    std::cerr <<"qgotabimageview4d requires 2 arguments:" <<std::endl;
+    std::cerr <<"1-filename(.jpg)" <<std::endl;
+    std::cerr <<"2-test (boolean)" <<std::endl;
+    return EXIT_FAILURE;
+    }
+
   QApplication app( argc, argv );
   QCoreApplication::setOrganizationName( "MegasonLab" );
   QCoreApplication::setOrganizationDomain( "http://gofigure2.sourceforge.net" );
