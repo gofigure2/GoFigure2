59e05051cf0fbf183f09a9672703e0ac3216fd6a
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2009-10-19 21:55:09 +0000

    Add class QGoChannelVisualizationDockWidget
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@1612 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

---
+++ branches/GoFigure2Beta/Code/GUI/lib/QGoChannelVisualizationDockWidget.cxx
@@ -0,0 +1,89 @@
+#include "QGoChannelVisualizationDockWidget.h"
+
+QGoChannelVisualizationDockWidget::
+QGoChannelVisualizationDockWidget( QWidget* parent ) : QDockWidget( parent )
+{
+  QObject::connect( this->buttonBox, SIGNAL( accepted() ),
+    this, SLOT( Accept() ) );
+}
+
+QGoChannelVisualizationDockWidget::
+~QGoChannelVisualizationDockWidget()
+{
+}
+
+void
+QGoChannelVisualizationDockWidget::
+SetNumberOfChannels( const unsigned int& iN )
+{
+  this->ChannelComboBox->setMaxCount( iN );
+}
+
+void
+QGoChannelVisualizationDockWidget::
+SetChannel( const unsigned int& i, const QString& iText )
+{
+  QString input = QString( "Ch.%1" ).arg( i );
+
+  if( !iText.isEmpty() )
+    {
+    input.append( iText );
+    }
+  this->ChannelComboBox->insertItem( i, input );
+}
+
+void
+QGoChannelVisualizationDockWidget::
+AddChannel( const QString& iText )
+{
+  int n = this->ChannelComboBox->count();
+
+  QString input = QString( "Ch.%1" ).arg( n );
+
+  if( !iText.isEmpty() )
+    {
+    input.append( iText );
+    }
+  this->ChannelComboBox->addItem( input );
+}
+
+void QGoChannelVisualizationDockWidget::
+Accept()
+{
+  int idx = this->ChannelComboBox->currentIndex();
+  if( idx == -1 )
+    {
+    return;
+    }
+
+  unsigned int temp = static_cast< unsigned int >( idx );
+
+  if( this->AllChannelsBtn->isChecked() )
+    {
+    if( !m_AllChannels )
+      {
+      m_AllChannels = true;
+      emit ChangeVisualizationToAllChannels();
+      }
+    }
+  else
+    {
+    if( m_AllChannels )
+      {
+      m_AllChannels = false;
+      m_ChannelId = temp;
+      emit ChangeVisualizationToOneChannel( temp );
+      }
+    else
+      {
+      if( temp != m_ChannelId )
+        {
+        m_AllChannels = false;
+        m_ChannelId = temp;
+        emit ChangeVisualizationToOneChannel( temp );
+        }
+      }
+    }
+}
+
+
