009a49b823aaaf69f975902868ef9a946441a591
Lydie Souhait <lydie_souhait@hms.harvard.edu>
2009-10-29 18:50:09 +0000

    add examples from wizard
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@1650 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

---
+++ branches/GoFigure2Beta/Examples/GUI/lib/QGoWizardDBTest.cxx
@@ -0,0 +1,24 @@
+#include <qapplication.h>
+#include "QGoWizardDB.h"
+
+ 
+ int main(int argc, char *argv[])
+ {
+     //Q_INIT_RESOURCE(qgocreatedb);
+
+     QApplication app(argc, argv);
+     QGoWizardDB* wizard = new QGoWizardDB;
+     wizard->show();
+     int output = app.exec();
+     std::vector<std::vector<std::string> > filenames = wizard->GetFilenamesFromDB();
+     for (unsigned int i =0; i < filenames.size(); i++)
+       {
+       for (unsigned int j = 0; j < filenames[i].size();j++)
+         {
+         std::cout<<"image filename with channel "<<i<<filenames[i][j].c_str()<<std::endl;
+         }
+       }
+
+     delete wizard;
+     return output;
+ }
