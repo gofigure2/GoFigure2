798fb55e79d149535ccce1ec0947d868c53cfd54
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2010-01-05 19:00:25 +0000

    Testings for recorder without signals
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@1956 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

---
+++ trunk/GoFigure2Beta/Examples/GUI/lib/QGoVideoRecorderTest.cxx
@@ -0,0 +1,41 @@
+#include <QApplication>
+#include <QTimer>
+#include "QGoVideoRecorder.h"
+
+//**************************************************************************//
+//                               MAIN                                       //
+//**************************************************************************//
+
+int main( int argc, char *argv[] )
+  {
+	if( argc != 2 )
+	    {
+	    std::cout <<"Usage : ./VideoRecorderTest " <<std::endl;
+	    std::cout << "1-test (boolean)" <<std::endl;
+	    return EXIT_FAILURE;
+	    }
+
+  QApplication app(argc, argv);
+  QTimer* timer = new QTimer;
+    timer->setSingleShot( true );
+
+  QGoVideoRecorder window(NULL);
+
+  QObject::connect( timer, SIGNAL( timeout() ), &window, SLOT( accept() ) );
+
+    if( atoi( argv[1] ) == 1 )
+      {
+      timer->start( 1000 );
+      }
+
+  window.show();
+
+  app.processEvents();
+  app.exec();
+
+  app.closeAllWindows();
+
+  delete timer;
+
+  return 0;
+  }
