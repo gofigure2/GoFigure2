c77b0bf553706d59fb2b3faa297b72a869d8e871
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2010-02-04 16:42:56 +0000

    ENH: Add QGoPrintDatabaseTest into the ctest list
    git-svn-id: svn+ssh://orchestra.med.harvard.edu/svn/megason/Code/GoFigure2@2076 ef826fda-c0ae-4237-af8e-f8dcbcf7929c

--- trunk/GoFigure2Beta/Examples/GUI/lib/QGoPrintDatabaseTest.cxx
+++ trunk/GoFigure2Beta/Examples/GUI/lib/QGoPrintDatabaseTest.cxx
@@ -1,23 +1,86 @@
+/*=========================================================================
+  Author: $Author$  // Author of last commit
+  Version: $Rev$  // Revision of last commit
+  Date: $Date$  // Date of last commit
+=========================================================================*/
+
+/*=========================================================================
+ Authors: The GoFigure Dev. Team.
+ at Megason Lab, Systems biology, Harvard Medical school, 2009
+
+ Copyright (c) 2009, President and Fellows of Harvard College.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ Neither the name of the  President and Fellows of Harvard College
+ nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written
+ permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#include <QApplication>
+#include <QTimer>
+
 #include "QGoPrintDatabase.h"
 
 
 int main(int argc, char * argv[])
 {
- /* if( argc != 2 )
+  if( argc != 2 )
     {
-    return false;
-    }*/
+    std::cerr <<"QGoPrintDatabaseTest requires 1 argument:" <<std::endl;
+    std::cerr <<"1-test (boolean)" <<std::endl;
+    return EXIT_FAILURE;
+    }
 
-  QApplication app(argc, argv);
+  QApplication app( argc, argv );
+  QCoreApplication::setOrganizationName("MegasonLab");
+  QCoreApplication::setOrganizationDomain( "http://gofigure2.sourceforge.net" );
 
-  QGoPrintDatabase win;
+  QGoPrintDatabase* win = new QGoPrintDatabase;
  // win.FillTableFromDatabase(argv[1],"localhost","gofigure",
   //    "gofigure",atoi(argv[2]), argv[3]);
-  win.SetDatabaseVariables(
+  win->SetDatabaseVariables(
     "gofiguredatabase","localhost","gofigure",
    "gofigure",8, "LSM_Converter");
-  win.FillTableFromDatabase();
-  win.show();
+  win->FillTableFromDatabase();
+  win->show();
 
-  return app.exec();
+  QTimer* timer = new QTimer;
+  timer->setSingleShot( true );
+  QObject::connect( timer, SIGNAL( timeout() ), win, SLOT( close() ) );
+
+  if( atoi( argv[1] ) == 1 )
+    {
+    timer->start( 1000 );
+    }
+
+  int output = app.exec();
+
+  app.closeAllWindows();
+
+  delete win;
+  delete timer;
+
+  return output;
 }
