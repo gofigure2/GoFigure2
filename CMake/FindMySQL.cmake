d5a8e4e67866a211f815a216fbe084f479d1ea5f
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2010-04-20 14:11:39 +0000

    ENH: Improve FindMySQL.cmake no more links to MySQL5.0 and for mac (manually installed)

--- CMake/FindMySQL.cmake
+++ CMake/FindMySQL.cmake
@@ -23,11 +23,12 @@ FIND_PATH(MYSQL_INCLUDE_DIRECTORIES mysql.h
   /usr/local/include
   /usr/local/include/mysql
   /usr/local/mysql/include
+  /usr/local/mysql/include/mysql
   "C:/Program Files/MySQL/include"
-  "C:/Program Files/MySQL/MySQL Server 5.0/include"
+  "C:/Program Files/MySQL/MySQL Server 5.1/include"
   "C:/MySQL/include"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.0;Location]/include"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.0;Location]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.1;Location]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.1;Location]/include"
   DOC "Specify the directory containing mysql.h."
 )
 
@@ -35,15 +36,18 @@ FIND_LIBRARY( MYSQL_LIBRARY
   NAMES mysql libmysql mysqlclient
   PATHS
   /usr/lib
+  /usr/lib64
   /usr/lib/mysql
+  /usr/lib64/mysql
   /usr/local/lib
   /usr/local/lib/mysql
   /usr/local/mysql/lib
+  /usr/local/mysql/lib/mysql
   "C:/Program Files/MySQL/lib"
-  "C:/Program Files/MySQL/MySQL Server 5.0/lib/opt"
+  "C:/Program Files/MySQL/MySQL Server 5.1/lib/opt"
   "C:/MySQL/lib/debug"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.0;Location]/lib/opt"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.0;Location]/lib/opt"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MySQL AB\\MySQL Server 5.1;Location]/lib/opt"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MySQL AB\\MySQL Server 5.1;Location]/lib/opt"
   DOC "Specify the mysql library here."
 )
 
@@ -53,7 +57,7 @@ FIND_LIBRARY( MYSQL_LIBRARY
 IF (NOT WIN32)
   FIND_LIBRARY( MYSQL_EXTRA_LIBRARIES
                 NAMES z zlib
-                PATHS /usr/lib /usr/local/lib 
+                PATHS /usr/lib /usr/lib64 /usr/local/lib 
                 DOC "If more libraries are necessary to link in a MySQL client (typically zlib), specify them here.")
 ELSE (NOT WIN32)
   SET( MYSQL_EXTRA_LIBRARIES "" )
