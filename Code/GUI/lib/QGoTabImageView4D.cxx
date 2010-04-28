fb2611adf94cef64af756764174bfd02518e7b0f
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2010-04-28 19:28:11 +0000

    COMP: intialize value (test continuous)

--- Code/GUI/lib/QGoTabImageView4D.cxx
+++ Code/GUI/lib/QGoTabImageView4D.cxx
@@ -1097,7 +1097,7 @@ void
 QGoTabImageView4D::
 ChangeContourRepresentationProperty()
 {
-  double linewidth;// = m_ManualSegmentationDockWidget->GetLinesWidth();
+  double linewidth = 0.0;// = m_ManualSegmentationDockWidget->GetLinesWidth();
   QColor linecolor;// = m_ManualSegmentationDockWidget->GetLinesColor();
   QColor nodecolor;// = m_ManualSegmentationDockWidget->GetNodesColor();
   QColor activenodecolor;// = m_ManualSegmentationDockWidget->GetActiveNodesColor();
