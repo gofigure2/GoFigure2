a77fd1375bc60bdd8f6ae34db12658141718aa13
Antonin Perrot-Audet <antonin07130@gmail.com>
2010-06-04 17:50:29 +0000

    Started modifying style for accordance with Gofigure2 project

--- Code/GUI/lib/QGoComparer.h
+++ Code/GUI/lib/QGoComparer.h
@@ -1,3 +1,42 @@
+/*=========================================================================
+  Author: $Author$  // Author of last commit
+  Version: $Rev$  // Revision of last commit
+  Date: $Date$  // Date of last commit
+=========================================================================*/
+
+/*=========================================================================
+ Authors: The GoFigure Dev. Team.
+ at Megason Lab, Systems biology, Harvard Medical school, 2009-10
+
+ Copyright (c) 2009-10, President and Fellows of Harvard College.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ Neither the name of the  President and Fellows of Harvard College
+ nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written
+ permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
 #ifndef QGOCOMPARER_H
 #define QGOCOMPARER_H
 /*
@@ -19,55 +58,58 @@ class QGoCompareOrchestra;
 class QGoComparer : public QWidget, protected Ui::QGoComparer {
     Q_OBJECT
 public:
-    QGoComparer(QString iComparerName, QWidget *parent = 0);
+  QGoComparer(QString iComparerName, QWidget *iParent = 0);
 
-    ~QGoComparer();
+  /** \brief Destructor. */
+  ~QGoComparer();
 
-    /** Set image displayed by the comparer
-    */
-    virtual void SetImage(vtkImageData* iImage);
+  /** \brief Set image displayed by the comparer
+  */
+  virtual void SetImage(vtkImageData* iImage);
 
+  /** \brief returns the type of comparer (2 for 2D, 3 for 3D)
+  */
+  virtual int GetComparerType( void )=0;
 
+  /** \brief get comparer's name
+  */
+  QString* GetName( void );
 
-    // returns the type of comparer (2 for 2D, 3 for 3D)
-    virtual int GetComparerType()=0;
+  /** \brief Update the viewer contained in the widget
+  */
+  virtual void Update( void );
 
-    /** get comparer's name
-    */
-    QString* GetName();
+  /** \brief render the viewer contained in the widget if any
+  */
+  virtual void Render( void );
 
-    /**Update the viewer contained in the widget
-    */
-    virtual void Update();
+  // get the camera of the current viewer;
+  virtual vtkCamera* GetCamera( void );
 
-    /** render the viewer contained in the widget if any
-    */
-    virtual void Render();
+  /** \brief print the comparer information :
+  *  it consists in the image information if any.
+  */
+  virtual void PrintOs(ostream &os) = 0;
 
-    // get the camera of the current viewer;
-    virtual vtkCamera* GetCamera();
+  /** \brief true if the widget has a viewer
+  */
+  bool HasViewer( void );
 
-    /* print the comparer information :
-    *  it consists in the image information if any.
-    */
-    virtual void PrintOs(ostream &os) = 0;
-
-    /** true if the widget has a viewer
-    */
-    bool HasViewer();
-
-    /** Set the address of the current orchestra
-    */
-    void SetCurrentOrchestra(QGoCompareOrchestra* iCurrentOrchestra);
+  /** \brief Set the address of the current orchestra
+  */
+  void SetCurrentOrchestra(QGoCompareOrchestra* iCurrentOrchestra);
 
 public slots:
-
-    virtual QString SnapshotViewXY( const GoFigure::FileType& iType,
+  /** \brief Save a snapshot of the displaid view, in a iType file
+  */
+  virtual QString SnapshotViewXY( const GoFigure::FileType& iType,
     const QString& iBaseName = tr( "Snapshot" ) ) = 0;
 
 
 protected:
-    void changeEvent(QEvent *e);
+  /** \brief Qt change event function
+  */
+  void changeEvent(QEvent *e);
 
   QString                           m_currentComparerName;
   vtkImageData*                     m_currentImage;
@@ -75,25 +117,19 @@ protected:
   QGoImageView*                     m_currentView;
   QGoCompareOrchestra*              m_currentOrchestra;
   vtkEventQtSlotConnect*            m_VTKEventQtConnector;
-private slots:
 
+private slots:
   // the QMetaObject::connectSlotsByName(QGoComparer)
   // is provided in the Setupui generated by moc
 
-
-
-
 private:
+  /** delete the viewer contained in the widget
+  */
+  void deleteViewer( void );
 
-
-  // delete the viewer contained in the widget
-  void deleteViewer();
-  // create the viewer contained in the widget
-  virtual void createViewer() = 0;
-
-
-
-
+  /** create the viewer contained in the widget
+  */
+  virtual void createViewer( void ) = 0;
 };
 
 #endif // QGOCOMPARER_H
