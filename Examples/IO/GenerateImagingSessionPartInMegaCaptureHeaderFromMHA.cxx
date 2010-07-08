773870388c4681ea01d8b474faa06db3ecd413c0
Kishore Mosaliganti <kishoreraom@gmail.com>
2010-07-08 17:34:31 +0000

    ENH: Made the number of channels change in generating the megacapture file

--- Examples/IO/GenerateImagingSessionPartInMegaCaptureHeaderFromMHA.cxx
+++ Examples/IO/GenerateImagingSessionPartInMegaCaptureHeaderFromMHA.cxx
@@ -45,12 +45,13 @@
 
 int main( int argc, char** argv )
 {
-  if( argc != 5 )
+  if( argc != 6 )
     {
     std::cerr <<"1-mha filename" <<std::endl;
     std::cerr <<"2-megacapture header (*.meg)" <<std::endl;
     std::cerr <<"3-Time Interval (in sec)" <<std::endl;
     std::cerr <<"4-Number of time points" <<std::endl;
+    std::cerr <<"5-Number of channels" <<std::endl;
     return EXIT_FAILURE;
     }
 
@@ -71,6 +72,7 @@ int main( int argc, char** argv )
 
   double iTimeInterval = atof( argv[3] );
   int iNumberOfTimePoints = atoi( argv[4] );
+  int iNumberOfChannels = atoi( argv[5] );
 
   std::ofstream file( argv[2] );
   file <<"MegaCapture" <<std::endl;
@@ -93,20 +95,30 @@ int main( int argc, char** argv )
   file <<"DimensionXT 1" <<std::endl;
   file <<"DimensionTM " <<iNumberOfTimePoints <<std::endl;
   file <<"DimensionZS " <<dim[2] <<std::endl;
-  file <<"DimensionCH 3" <<std::endl;
 
-  int red = 255;
-  int green = 0;
-  int blue = 0;
-  file <<"ChannelColor00 " << red * 256 * 256 + green * 256 + blue <<std::endl;
-  red = 0;
-  green = 255;
-  blue = 0;
-  file <<"ChannelColor01 " << red * 256 * 256 + green * 256 + blue <<std::endl;
-  red = 0;
-  green = 0;
-  blue = 255;
-  file <<"ChannelColor02 " << red * 256 * 256 + green * 256 + blue <<std::endl;
+
+  if ( iNumberOfChannels == 1 )
+    {
+    file <<"DimensionCH 1" <<std::endl;
+    int blue = 255;
+    file <<"ChannelColor00 " << blue <<std::endl;
+    }
+  else
+    {
+    file <<"DimensionCH 3" <<std::endl;
+    int red = 255;
+    int green = 0;
+    int blue = 0;
+    file <<"ChannelColor00 " << red * 256 * 256 + green * 256 + blue <<std::endl;
+    red = 0;
+    green = 255;
+    blue = 0;
+    file <<"ChannelColor01 " << red * 256 * 256 + green * 256 + blue <<std::endl;
+    red = 0;
+    green = 0;
+    blue = 255;
+    file <<"ChannelColor02 " << red * 256 * 256 + green * 256 + blue <<std::endl;
+    }
 
   file <<"ChannelDepth 8" <<std::endl;
   file <<"FileType PNG" <<std::endl;
