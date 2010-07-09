5cd151f2c6de1a3ebc623bcde97aeb7563e8a11d
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2010-07-09 18:26:37 +0000

    ENH: "bool Visibility" variable added in the contour mesh structure TODO: update all the methods using the contour mesh structure to take into account this new parameter

--- Code/IO/ContourMeshStructure.h
+++ Code/IO/ContourMeshStructure.h
@@ -56,14 +56,22 @@ struct QGOIO_EXPORT ContourMeshStructure
   unsigned int        CollectionID;
   unsigned int        TCoord;
   bool                Highlighted;
+  bool                Visibility;
   double              rgba[4];
   int                 Direction;
 
   ContourMeshStructure( );
+
+  // Obsolete...
   ContourMeshStructure( const unsigned int& iTraceID, vtkActor* iActor,
     vtkPolyData* iNodes, const unsigned int& iCollectionID, const unsigned int& iT,
     const bool& iHighlighted, const double& r, const double& g, const double& b,
     const double& alpha, const int& iDir );
+
+  ContourMeshStructure( const unsigned int& iTraceID, vtkActor* iActor,
+    vtkPolyData* iNodes, const unsigned int& iCollectionID, const unsigned int& iT,
+    const bool& iHighlighted, const bool& iVisibility, const double& r,
+    const double& g, const double& b, const double& alpha, const int& iDir );
 
   ContourMeshStructure( const ContourMeshStructure& iE );
   ~ContourMeshStructure();
@@ -76,6 +84,7 @@ struct QGOIO_EXPORT ContourMeshStructure
     os <<"CollectionID " <<c.CollectionID <<std::endl;
     os <<"TCoord " <<c.TCoord <<std::endl;
     os <<"Highlighted " <<c.Highlighted <<std::endl;
+    os <<"Visibility " <<c.Visibility <<std::endl;
     os <<"RGBA [" <<c.rgba[0] <<", " <<c.rgba[1] <<", " <<c.rgba[2]
        <<", " <<c.rgba[3] <<"]" <<std::endl;
     os <<"Direction " <<c.Direction <<std::endl;
