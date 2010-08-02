251c6ae256f04584e43d84cb7b7024b28d64435c
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2010-08-02 17:00:14 +0000

    STYLE: fix copyright in vtkPoissonReconstruction code

--- Code/ExternalCode/PoissonReconstruction/vtkNormalEstimationFilter.h
+++ Code/ExternalCode/PoissonReconstruction/vtkNormalEstimationFilter.h
@@ -31,6 +31,22 @@
  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  =========================================================================*/
+ 
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkSurfaceReconstructionFilter.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
 
 // .NAME vtkNormalEstimationFilter - estimate normals from unorganized points
 // .SECTION Description
--- Code/ExternalCode/PoissonReconstruction/vtkPoissonReconstruction.h
+++ Code/ExternalCode/PoissonReconstruction/vtkPoissonReconstruction.h
@@ -1,23 +1,24 @@
 /*=========================================================================
- Modifications were made by the GoFigure Dev. Team.
- while at Megason Lab, Systems biology, Harvard Medical school, 2009-10
-
- Copyright (c) 2009-10, President and Fellows of Harvard College.
- All rights reserved.
-
+ Authors: David Doria at Rensselaer Polytechnic Institute and
+   Arnaud Gelas at Harvard Medical School
+ 	  
+ Copyright (c) 2010, David Doria at Rensselaer Polytechnic Institute and
+   Arnaud Gelas at Harvard Medical School,
+   All rights reserved.
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
-
+ 
  Redistributions of source code must retain the above copyright notice,
  this list of conditions and the following disclaimer.
  Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- Neither the name of the  President and Fellows of Harvard College
- nor the names of its contributors may be used to endorse or promote
- products derived from this software without specific prior written
- permission.
-
+ Neither the name of the Rensselaer Polytechnic Institute and of Harvard
+ Medical School nor the names of its contributors may be used to endorse 
+ or promote products derived from this software without specific prior 
+ written permission.
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -29,8 +30,7 @@
  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
- =========================================================================*/
+=========================================================================*/
 
 #ifndef __vtkPoissonReconstruction_h
 #define __vtkPoissonReconstruction_h
