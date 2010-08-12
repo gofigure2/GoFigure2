d6fcb42787a697b894f75dd6e929633806910f68
Antonin Perrot-Audet <antonin07130@gmail.com>
2010-08-12 18:45:58 +0000

    ENH : Added QGoSynchronized Examples, deleted qgosynchronizedguitest

---
+++ Examples/GUI/lib/compareexample.cxx
@@ -0,0 +1,115 @@
+/*=========================================================================
+  Author: $Author$  // Author of last commit
+  Version: $Rev$  // Revision of last commit
+  Date: $Date$  // Date of last commit
+=========================================================================*/
+
+/*=========================================================================
+ Authors: The GoFigure Dev. Team.
+ at Megason Lab, Systems biology, Harvard Medical school, 2009
+
+ Copyright (c) 2009, President and Fellows of Harvard College.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ Neither the name of the  President and Fellows of Harvard College
+ nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written
+ permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#include <QApplication>
+#include "vtkImageReader2Factory.h"
+#include "vtkImageReader2.h"
+#include "vtkSmartPointer.h"
+#include "vtkImageReader2Collection.h"
+
+#include "QGoSynchronizedViewManager.h"
+
+
+/* This simple examples shows how to use the high level interface
+ * of the QGoSynchronize classes.
+ * It takes a list of images as an input and displays the readable ones
+ * with the standard VTK reader.
+ * It synchronize the visualizations.
+ */
+
+int main(int argc, char** argv)
+{
+  QApplication app(argc, argv);
+  QCoreApplication::setOrganizationName("MegasonLab");
+  QCoreApplication::setOrganizationDomain("http://gofigure2.sourceforge.net");
+
+  vtkSmartPointer< vtkImageReader2Factory > imageFactory =
+    vtkSmartPointer< vtkImageReader2Factory >::New();
+
+  /* we simply create a new manager that will take care of
+   ∗ creation/deletion of visualization and callbacks for us.
+   */
+  QGoSynchronizedViewManager* ViewManager	=	new	QGoSynchronizedViewManager ();
+
+  QString ViewName;
+
+  for (int i = 0; i<argc;++i)
+    {
+    std::cout << argv[i] << std::endl;
+
+    vtkImageReader2 * imageReader =
+       imageFactory->CreateImageReader2( argv[i] );
+
+    if (imageReader != NULL)
+      {
+      imageReader->SetFileName( argv[i] );
+      imageReader->Update();
+
+
+      ViewName.clear();
+      ViewName.append(argv[i]);
+
+      /* the synchronization manager can create visualization windows given
+       * a valid pointer to a VTK image and
+       * a string encoding the name of the visualization.
+       */
+      ViewManager->newSynchronizedView(ViewName, imageReader->GetOutput() );
+      ViewManager->Update();
+      ViewManager->show();
+      }
+    else
+      {
+      std::cerr << argv[i]
+                << "is not a readable image by standard vtk reader"
+                << std::endl;
+      }
+    }
+
+/* the synchronization manager can synchronize the opened images
+∗ with a simple function call */
+ViewManager->synchronizeOpenSynchronizedViews();
+
+app.processEvents();
+int output = app.exec();
+
+delete ViewManager;
+
+return output;
+}
--- Examples/GUI/lib/compareguiexample.cxx
+++ Examples/GUI/lib/compareguiexample.cxx
@@ -38,102 +38,18 @@
 
 =========================================================================*/
 #include <QApplication>
-#include <QTimer>
-
-#include "vtkSmartPointer.h"
-#include "vtkPNGReader.h"
-#include "vtkImageGaussianSmooth.h"
-#include "vtkMetaImageReader.h"
-/*
-#include "QGoSynchronizedView.h"
-#include  "QGoSynchronizedViewManager.h"
-#include "vtkMetaImageReader.h"
-#include "QGoSynchronizedView3D.h"
-*/
 #include "QGoSynchronizedViewMainWindow.h"
 
-#include <QStringList>
-#include <QString>
-
-#include "itkImageFileReader.h"
-#include "itkImage.h"
 
 int main(int argc, char** argv)
 {
-
-  if (argc != 4)
-    {
-    std::cout << "Usage : QGoSynchronizedViewGUITest(.exe) " << std::endl;
-    std::cout << "1-file.png" << std::endl;
-    std::cout << "2-file.mhd" << std::endl;
-    std::cout << "3-test (boolean)" << std::endl;
-    return EXIT_FAILURE;
-    }
-
   QApplication app(argc, argv);
   QCoreApplication::setOrganizationName("MegasonLab");
   QCoreApplication::setOrganizationDomain("http://gofigure2.sourceforge.net");
 
-  // ITK Typedefs
-  // ITK Reader Typedef
-  typedef double InputPixelType;
-  const unsigned int Dimension = 3;
-  typedef itk::Image<InputPixelType, Dimension> InputImage3DType;
-  typedef InputImage3DType::Pointer             InputImage3DPointer;
-
-  //itk reader
-  typedef itk::ImageFileReader<InputImage3DType> itkReaderType;
-  itkReaderType::Pointer itkReader = itkReaderType::New();
-  itkReader->SetFileName(argv[2]);
-  itkReader->Update();
-
-  // create 2 2D images from 1
-  vtkSmartPointer<vtkPNGReader> reader = vtkSmartPointer<vtkPNGReader>::New();
-  reader->SetFileName(argv[1]);
-  reader->Update();
-
-  vtkSmartPointer<vtkImageGaussianSmooth> filter1 =
-    vtkSmartPointer<vtkImageGaussianSmooth>::New();
-  filter1->SetInputConnection(reader->GetOutputPort());
-  filter1->Update();
-
-  // create 2 3D images from 1
-  vtkSmartPointer<vtkMetaImageReader> reader3D = vtkSmartPointer<vtkMetaImageReader>::New();
-  reader3D->SetFileName(argv[2]);
-  reader3D->Update();
-
-  vtkSmartPointer<vtkImageGaussianSmooth> filter13D =
-    vtkSmartPointer<vtkImageGaussianSmooth>::New();
-  filter13D->SetInputConnection(reader3D->GetOutputPort());
-  filter13D->Update();
-
-  QString cp0 = "comp0";
-  QString cp1 = "comp1";
-
-  QString cp03D = "comp03D";
-  QString cp13D = "comp13D";
-  QString cp23D = "compITK13D";
-  QTimer* timer = new QTimer;
-  timer->setSingleShot(true);
 
   QGoSynchronizedViewMainWindow* SynchronizedViewMainWindow
     = new QGoSynchronizedViewMainWindow();
-
-  QObject::connect(timer, SIGNAL(timeout()), qApp, SLOT(closeAllWindows()));
-
-  if (atoi(argv[3]) == 1) timer->start(2000);
-
-  // open some images for the test
-  SynchronizedViewMainWindow->newSynchronizedView(
-    cp0, reader->GetOutput());
-  SynchronizedViewMainWindow->newSynchronizedView(
-    cp1, filter1->GetOutput());
-  SynchronizedViewMainWindow->newSynchronizedView(
-    cp03D, reader3D->GetOutput());
-  SynchronizedViewMainWindow->newSynchronizedView(
-    cp13D, filter13D->GetOutput());
-  SynchronizedViewMainWindow->newSynchronizedView<InputPixelType>(
-    cp23D, itkReader->GetOutput());
 
   SynchronizedViewMainWindow->Update();
   SynchronizedViewMainWindow->show();
@@ -142,5 +58,4 @@ int main(int argc, char** argv)
   int output = app.exec();
   return output;
 
-  delete timer;
 }
