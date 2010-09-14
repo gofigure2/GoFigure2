551b8c7e4ed189629b543854f6411aad1e4bd5ad
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2010-09-14 19:55:57 +0000

    ENH: removed useless call to "Render()"

--- Code/GUI/lib/QGoImageView3D.cxx
+++ Code/GUI/lib/QGoImageView3D.cxx
@@ -797,7 +797,7 @@ AddContour(vtkPolyData* dataset, vtkProperty* iProperty)
   vtkActor* temp = m_View3D->AddDataSet((vtkDataSet*) dataset,
                                         iProperty, false, false);
 
-  m_View3D->Render();
+  //m_View3D->Render();
   oList.push_back(temp);
 
   return oList;
