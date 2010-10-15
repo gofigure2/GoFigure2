5f770887e6fe8be4d42b7ca7b874deaae98bdeb3
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2010-10-15 14:16:11 +0000

    ENH: Take uncrustify configuration file from ITK's project

--- KWStyle/uncrustify.sh
+++ KWStyle/uncrustify.sh
@@ -4,13 +4,13 @@
 # =========================================================================
 #  Modifications were made by the GoFigure Dev. Team.
 #  while at Megason Lab, Systems biology, Harvard Medical school, 2009-10
-# 
+#
 #  Copyright (c) 2009-10, President and Fellows of Harvard College.
 #  All rights reserved.
-# 
+#
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are met:
-# 
+#
 #  Redistributions of source code must retain the above copyright notice,
 #  this list of conditions and the following disclaimer.
 #  Redistributions in binary form must reproduce the above copyright notice,
@@ -20,7 +20,7 @@
 #  nor the names of its contributors may be used to endorse or promote
 #  products derived from this software without specific prior written
 #  permission.
-# 
+#
 #  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 #  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -32,55 +32,61 @@
 #  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 #  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 #  =========================================================================
-# 
+#
 #
 # Linux Shell Script to run ITK tests in linux
 #
-command=$1;
-working_directory=`pwd`;
-configuration_file="$working_directory"/uncrustify.cfg;
-cd ..;
+
+function local_uncrustify {
+configuration_file=$1;
+directory=$2;
 
 # find cxx files
-for i in `find . -name *.cxx`;
+for i in `find $directory -name *.cxx`;
 do
    echo "Correcting file style: $i";
-   $command -c $configuration_file -f $i -o $i;
+   uncrustify -c $configuration_file -f $i -o $i;
    echo "Done";
 done;
 
 # find cxx.in files
-for i in `find . -name *.cxx.in`;
+for i in `find $directory -name *.cxx.in`;
 do
    echo "Correcting file style: $i";
-   $command -c $configuration_file -f $i -o $i;
+   uncrustify -c $configuration_file -f $i -o $i;
    echo "Done";
 done;
 
 # find h files
-for i in `find . -name *.h`;
+for i in `find $directory -name *.h`;
 do
    echo "Correcting file style: $i";
-   $command -c $configuration_file -f $i -o $i;
+   uncrustify -c $configuration_file -f $i -o $i;
    echo "Done";
 done;
 
 # find h.in files
-for i in `find . -name *.h.in`;
+for i in `find $directory -name *.h.in`;
 do
    echo "Correcting file style: $i";
-   $command -c $configuration_file -f $i -o $i;
+   uncrustify -c $configuration_file -f $i -o $i;
    echo "Done";
 done;
 
 # find txx files
-for i in `find . -name *.txx`;
+for i in `find $directory -name *.txx`;
 do
    echo "Correcting file style: $i";
-   $command -c $configuration_file -f $i -o $i;
+   uncrustify -c $configuration_file -f $i -o $i;
    echo "Done";
 done;
+}
 
-echo "Project's style updated"
+working_directory=`pwd`;
+configuration_file="$working_directory"/uncrustify.cfg;
+
+local_uncrustify $configuration_file "$working_directory"/../Code
+local_uncrustify $configuration_file "$working_directory"/../Examples
+
