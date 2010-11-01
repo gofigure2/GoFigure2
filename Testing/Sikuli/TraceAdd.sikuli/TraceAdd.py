3b3b4a0d336075440a094e9f7392ca94f51b71ed
Evan Schwab <eschwab8@gmail.com>
2010-11-01 13:07:14 -0400

    deleted images from TraceAdd folder and put them into gofigure2_images with new descriptive names

--- Testing/Sikuli/TraceAdd.sikuli/TraceAdd.py
+++ Testing/Sikuli/TraceAdd.sikuli/TraceAdd.py
@@ -1,6 +1,8 @@
 import os
 address = os.getcwd()
-address = address+"/TraceAdd.sikuli"
+#address should be ~/GITROOT/GoFigure2/Testing/Sikuli
+
+address = address+"/gofigure2_images"
 print address
 setBundlePath(address)
 
@@ -17,13 +19,13 @@ def AddNew(Region):
 
 #x = find("Cell Contour")
 #print x
-TraceObject = ("1285599876548.png","1287655664410.png","1287655672392.png")
+TraceObject = ("SelectedColor.png","SelectedCellType.png","SelectedSubCellType.png")
 Action = ("Add New", "Cancel", "No Name", "Existing Name")
 #test 1: Add a new...
 for a in Action:
 		for i in TraceObject:
 			AddNew(i)
-			if i == ("1285599876548.png") :
+			if i == ("SelectedColor.png") :
 				click("colortable.png")
 				click("ok.png")
 			if a == "Add New":
@@ -34,21 +36,22 @@ for a in Action:
 				#assert new name and color comes up in mysql
 			if a == "Cancel":
 				click("cancel.png")
-				#assert previous state is found
+				
 			if a == "No Name":
-				click("ok.png")
+				click("ok.png)
 				wait(1)
-				#cannot use assertExist in sikuli editor
-				assertExist("1287741083818.png")
-				click("1285598344252.png")
-				click("1288125769063.png")
+				
+				onAppear("errorPopUp.png")
+				click("ok.png")
+				click("cancel.png")
+				assert exists()
 			if a == "Existing Name":
 				type("Widget Test")
-				click("1285598344252.png")
+				click("ok.png")
 				#cannot use assertExist in sikuli editor
-				assertExist("1287741123199.png")
-				click("1285598344252.png")
-				click("1288125769063.png")
+				onAppear("errorPopUp.png")
+				click("ok.png")
+				click("cancel.png")
 #call time elapse function
 
 #test 2: Add a new... and click cancel
