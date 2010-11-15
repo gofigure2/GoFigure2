88f634b0235c2677811a005e2b8ca0305ebc1022
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2010-11-15 16:47:32 -0500

    ENH: Add one method SetActorVisibility and SetActorProperties

--- Code/IO/TrackStructure.cxx
+++ Code/IO/TrackStructure.cxx
@@ -36,6 +36,7 @@
 
 #include <iostream>
 #include "vtkPolyData.h"
+#include "vtkActor.h"
 
 //--------------------------------------------------------------------------
 TrackStructure::
@@ -229,3 +230,49 @@ ReplaceElement(int iTime, double* iPoint)
   return deleteElement;
 }
 //--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+void TrackStructure::SetActorProperties( vtkProperty* iProperty ) const
+  {
+  if( iProperty )
+    {
+    if( this->ActorXY )
+      {
+      this->ActorXY->SetProperty( iProperty );
+      }
+    if( this->ActorXZ )
+      {
+      this->ActorXZ->SetProperty( iProperty );
+      }
+    if( this->ActorYZ )
+      {
+      this->ActorYZ->SetProperty( iProperty );
+      }
+    if( this->ActorXYZ )
+      {
+      this->ActorXYZ->SetProperty( iProperty );
+      }
+    }
+  }
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+void TrackStructure::SetActorVisibility( const bool& iVisible ) const
+{
+  if ( this->ActorXY )
+    {
+    this->ActorXY->SetVisibility(iVisible);
+    }
+  if ( this->ActorXZ )
+    {
+    this->ActorXZ->SetVisibility(iVisible);
+    }
+  if ( this->ActorYZ )
+    {
+    this->ActorYZ->SetVisibility(iVisible);
+    }
+  if ( this->ActorXYZ )
+    {
+    this->ActorXYZ->SetVisibility(iVisible);
+    }
+}
