b777af9f14363e98395a4e3be0f1c7bd381ca8a3
Evan Schwab <eschwab8@gmail.com>
2010-11-18 15:07:09 -0500

    ENH: setUpDatabase works.  Will need to make sure the name is the same as the bash script

--- Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
+++ Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
@@ -8,27 +8,21 @@ setBundlePath(address)
 
 
 click("Settings.png")
-click("Database_arrow.png")
+hover("Database_arrow.png")
 click("Set_up_database.png")
 wait(1)
-type("gofigure") 
-Key.TAB 
-type("gofigure")
+type("gofigure" +Key.TAB + "gofigure")
 click("Next_arrow.png")
+wait(1)
+type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
 click("ok.png")
-Key.BACKSPACE
 click("ok.png")
-click("ok.png")
-Key.TAB+Key.TAB
-type("First")
-Key.TAB+Key.TAB
-type("Last")
-Key.TAB+Key.ENTER
+wait(3)
+type("First"+Key.TAB+Key.TAB+"Last"+Key.TAB+Key.ENTER)
 click("ok.png")
 click("Next_arrow.png")
 wait(1)
-type("microscope")
-Key.TAB+Key.ENTER
+type("microscope"+Key.TAB+Key.ENTER)
 click("ok.png")
 click("Finish.png")
 click("ok.png")
