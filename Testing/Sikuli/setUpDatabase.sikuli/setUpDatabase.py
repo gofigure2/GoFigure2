221bf54f61f1d4f1d57c96c949007805d7e8c6c2
Evan Schwab <eschwab8@gmail.com>
2010-11-18 14:21:29 -0500

    Added images and finished setting up database

--- Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
+++ Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
@@ -1,8 +1,31 @@
-click("1290103471275.png")
-click("1290103511231.png")
-click("1290103522252.png")
+import os
+address = os.getcwd()
+#address should be ~/GITROOT/GoFigure2/Testing/Sikuli/gofigure2_images
+
+#address = address+"/gofigure2_images"
+print address
+setBundlePath(address)
+
+
+click("Settings.png")
+click("Database_arrow.png")
+click("Set_up_database.png")
+wait(1)
 type("gofigure") 
 Key.TAB 
 type("gofigure")
-click("1290103664404.png")
+click("Next_arrow.png")
 click("ok.png")
+Key.BACKSPACE
+click("ok.png")
+click("ok.png")
+Key.TAB+Key.TAB
+type("First")
+Key.TAB+Key.TAB
+type("Last")
+Key.TAB+Key.ENTER
+click("ok.png")
+click("Next_arrow.png")
+wait(1)
+type("microscope")
+Key.TAB+Key.ENTER
