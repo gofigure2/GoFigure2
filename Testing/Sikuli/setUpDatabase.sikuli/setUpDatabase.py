1bcbf5129ee9b86438223067c4ac8af3b5fe8575
Evan Schwab <eschwab8@gmail.com>
2010-11-29 17:31:18 -0500

    BUG:  gofigure crashes. after it asks for root password, user clicks cancel, then a popup comes up saying "there is a problem with existing user",user clicks ok and gofigure crashes.

--- Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
+++ Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
@@ -6,36 +6,40 @@ address = os.getcwd()
 print address
 setBundlePath(address)
 
-for create in 3:
+for create in [1,2]:
 	click("Settings.png")
 	hover("Database_arrow.png")
 	click("Set_up_database.png")
 	assert exists("Create_user_for_MySQL_DB.png")
 	if create == 1:
 		click("cancel.png")
-		assert not exists("Create_user_for_MySQL_DB.png")
+		assert not exists("Create_user_for_MySQL_DB.png",0)
 	if create == 2:
 		click("Next_arrow.png")
 		assert exists("Please_fill_all_fields.png")
 		click("ok.png")
-	if create == 3:
-		wait(1)
+		assert not exists("Please_fill_all_fields.png",0)
+		
+		type(Key.TAB+Key.TAB)
+		wait(2)
 		type("gofigure" +Key.TAB + "gofigure")
 		click("Next_arrow.png")
 		assert exists("Root_Password.png")
 	
-		for root in 2
-			if root == 1
-				click("cancel.png")
+		for root in [1,2]:
+			if root == 1:
+				type(Key.TAB+Key.ENTER)
 				assert exists("There_is_problem_existing_users.png")
+				click("ok.png")
+				#gofigure crashes after this step
 				assert not exists("Root_Password.png")
-			if root == 2
+			if root == 2:
 				wait(1)
 				type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
 				click("ok.png")
 				click("ok.png")
 
-"1291065478425.png"wait(3)
+wait(3)
 type("First"+Key.TAB+Key.TAB+"Last"+Key.TAB+Key.ENTER)
 click("ok.png")
 click("Next_arrow.png")
