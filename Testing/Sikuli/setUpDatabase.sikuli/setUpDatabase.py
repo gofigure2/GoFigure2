f688182ee29779db53a435c7c3e4900b55611a65
Evan Schwab <eschwab8@gmail.com>
2010-11-29 16:26:21 -0500

    ENH: now testing all buttons on database setup

--- Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
+++ Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
@@ -6,18 +6,36 @@ address = os.getcwd()
 print address
 setBundlePath(address)
 
+for create in 3:
+	click("Settings.png")
+	hover("Database_arrow.png")
+	click("Set_up_database.png")
+	assert exists("Create_user_for_MySQL_DB.png")
+	if create == 1:
+		click("cancel.png")
+		assert not exists("Create_user_for_MySQL_DB.png")
+	if create == 2:
+		click("Next_arrow.png")
+		assert exists("Please_fill_all_fields.png")
+		click("ok.png")
+	if create == 3:
+		wait(1)
+		type("gofigure" +Key.TAB + "gofigure")
+		click("Next_arrow.png")
+		assert exists("Root_Password.png")
+	
+		for root in 2
+			if root == 1
+				click("cancel.png")
+				assert exists("There_is_problem_existing_users.png")
+				assert not exists("Root_Password.png")
+			if root == 2
+				wait(1)
+				type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
+				click("ok.png")
+				click("ok.png")
 
-click("Settings.png")
-hover("Database_arrow.png")
-click("Set_up_database.png")
-wait(1)
-type("gofigure" +Key.TAB + "gofigure")
-click("Next_arrow.png")
-wait(1)
-type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
-click("ok.png")
-click("ok.png")
-wait(3)
+"1291065478425.png"wait(3)
 type("First"+Key.TAB+Key.TAB+"Last"+Key.TAB+Key.ENTER)
 click("ok.png")
 click("Next_arrow.png")
