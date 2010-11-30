8fbad50dc0d059e3f3acf38278284f39b8d91465
Evan Schwab <eschwab8@gmail.com>
2010-11-30 13:49:28 -0500

    ENH: set up database testing works to the end.  Still commented out bit that crashes

--- Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
+++ Testing/Sikuli/setUpDatabase.sikuli/setUpDatabase.py
@@ -32,26 +32,61 @@ for create in [1,2]:
 			#gofigure crashes at this step
 		#assert not exists("Root_Password.png")
 	
-		for i in [1,2]:
-			click("Next_arrow.png")
-			assert exists("Root_Password.png")
-			wait(1)
-			type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
-			click("ok.png")
-			assert exists("create_database_for_this_user.png")
-			if i == 1:
-				type(Key.ENTER)
-				assert not exists("create_database_for_this_user.png")
-			if i == 2:
-				click("ok.png")
+for i in [1,2]:
+	click("Next_arrow.png")
+	assert exists("Root_Password.png")
+	wait(2)
+	type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
+	click("ok.png")
+	assert exists("create_database_for_this_user.png")
+	if i == 1:
+		type(Key.ENTER) #cancel confirmation
+		assert not exists("create_database_for_this_user.png")
+	if i == 2:
+		click("ok.png")
+		wait(5)
+		click("Back_arrow.png")
+		click("Next_arrow.png")
+		type(Key.BACKSPACE)#+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE+Key.BACKSPACE)
+		click("ok.png")
+		click("ok.png")
+		
 
+wait(5)
+click("Next_arrow.png")
+assert exists("Please_create_one_author.png")
+click("ok.png")
 
-wait(3)
-type("First"+Key.TAB+Key.TAB+"Last"+Key.TAB+Key.ENTER)
+type(Key.TAB+Key.TAB+"First"+Key.TAB+Key.TAB+"Last"+Key.TAB+Key.ENTER)
 click("ok.png")
 click("Next_arrow.png")
+click("Back_arrow.png")
+type(Key.TAB+Key.TAB+Key.TAB+Key.ENTER)
+assert exists("There_is_already_an_Author.png")
+click("ok.png")
+click("Next_arrow.png")
+
+click("Create_microscope.png")
+assert exists("Please_enter_name_of_microscope.png")
+click("ok.png")
+
+keyDown(Key.SHIFT)
+type(Key.TAB)
+keyUp(Key.SHIFT)
+
 wait(1)
 type("microscope"+Key.TAB+Key.ENTER)
+assert exists("Your_microscope_created.png")
+click("ok.png")
+click("Create_microscope.png")
+assert exists("There_is_already_microscope_name.png")
 click("ok.png")
 click("Finish.png")
+assert exists("MySQL_user_and_DB_created.png")
 click("ok.png")
+assert not exists("Create_microscopes_for_gofigure_projects_page.png",0)
+
+click("Settings.png")
+hover("Database_arrow.png")
+click("Set_up_database.png")
+assert not exists("Create_user_for_MySQL_DB.png")
