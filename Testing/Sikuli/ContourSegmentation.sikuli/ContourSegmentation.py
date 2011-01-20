826e1a3f8c96dc55c00e7c3834ab8b38116ebbac
Evan Schwab <eschwab8@gmail.com>
2011-01-20 12:40:43 -0500

    ENH:  Made DrawContour function. Used in loop for all three views.

--- Testing/Sikuli/ContourSegmentation.sikuli/ContourSegmentation.py
+++ Testing/Sikuli/ContourSegmentation.sikuli/ContourSegmentation.py
@@ -6,36 +6,41 @@ address = os.getcwd()
 #print address
 setBundlePath(address)
 
+def DrawContour(View):
+	
+	xyView = find(Pattern(View).similar(0.30))
+	print xyView.x, xyView.y, xyView.h
+
+	startx = xyView.x + int(round((0.3)*xyView.w))
+	starty = xyView.y + int(round((0.6)*xyView.h))
+	click(Location(startx,starty))
+	print startx, starty
+	wait(1)
+	Secondx = startx+10
+	Secondy = starty+10
+	click(Location(Secondx,Secondy))
+	wait(1)
+	Thirdx = Secondx+10
+	Thirdy = Secondy -10
+	click(Location(Thirdx,Thirdy))
+	Forthx = Thirdx - 10
+	Forthy = Thirdy - 10
+	wait(1)
+	click(Location(Forthx,Forthy))
+	wait(1)
+
+	click(Location(startx,starty))
+	wait(1)
+
+
 click("ContourSegmentationIcon.png")
 click("TableWidgetIcon.png")
 wait(1)
 
-for view in [1,3]:
+ViewRegion = ("XY_ViewRegion.png","XZ_ViewRegion.png","YZ_ViewRegion.png")
 
+for view in ViewRegion:
 
+	DrawContour(view)
 
-xyView = find(Pattern("XY_ViewRegion.png").similar(0.30))
-print xyView.x, xyView.y, xyView.h
-
-startx = xyView.x + int(round((0.3)*xyView.w))
-starty = xyView.y + int(round((0.6)*xyView.h))
-click(Location(startx,starty))
-print startx, starty
-wait(1)
-Secondx = startx+10
-Secondy = starty+10
-click(Location(Secondx,Secondy))
-wait(1)
-Thirdx = Secondx+10
-Thirdy = Secondy -10
-click(Location(Thirdx,Thirdy))
-Forthx = Thirdx - 10
-Forthy = Thirdy - 10
-wait(1)
-click(Location(Forthx,Forthy))
-wait(1)
-
-click(Location(startx,starty))
-wait(1)
-
-#click("Validate.png")
+	#click("Validate.png")
