d16d0d56f41dc5cd7d828f3a9dd962c56529951f
Evan Schwab <eschwab8@gmail.com>
2011-01-20 16:00:47 -0500

    Updated ContourSegmentation

--- Testing/Sikuli/ContourSegmentation.sikuli/ContourSegmentation.py
+++ Testing/Sikuli/ContourSegmentation.sikuli/ContourSegmentation.py
@@ -8,39 +8,70 @@ setBundlePath(address)
 
 def DrawContour(View):
 	
-	xyView = find(Pattern(View).similar(0.30))
-	print xyView.x, xyView.y, xyView.h
+	View == "last":
+	
 
-	startx = xyView.x + int(round((0.3)*xyView.w))
-	starty = xyView.y + int(round((0.6)*xyView.h))
-	click(Location(startx,starty))
-	print startx, starty
-	wait(1)
-	Secondx = startx+10
-	Secondy = starty+10
-	click(Location(Secondx,Secondy))
-	wait(1)
-	Thirdx = Secondx+10
-	Thirdy = Secondy -10
-	click(Location(Thirdx,Thirdy))
-	Forthx = Thirdx - 10
-	Forthy = Thirdy - 10
-	wait(1)
-	click(Location(Forthx,Forthy))
-	wait(1)
+		xyView = find(Pattern(View).similar(0.30))
+		print xyView.x, xyView.y, xyView.h
 
-	click(Location(startx,starty))
-	wait(1)
+		startx = xyView.x + int(round((0.3)*xyView.w))
+		starty = xyView.y + int(round((0.6)*xyView.h))
+		click(Location(startx,starty))
+		print startx, starty
+		wait(1)
+		Secondx = startx+10
+		Secondy = starty+10
+		click(Location(Secondx,Secondy))
+		wait(1)
+		Thirdx = Secondx+10
+		Thirdy = Secondy -10
+		click(Location(Thirdx,Thirdy))
+		Forthx = Thirdx - 10
+		Forthy = Thirdy - 10
+		wait(1)
+		click(Location(Forthx,Forthy))
+		wait(1)
+	
+		click(Location(startx,starty))
+		wait(1)
 
 
 click("ContourSegmentationIcon.png")
 click("TableWidgetIcon.png")
 wait(1)
 
-ViewRegion = ("XY_ViewRegion.png","XZ_ViewRegion.png","YZ_ViewRegion.png")
+ViewRegion = ("XY_ViewRegion.png","XZ_ViewRegion.png","YZ_ViewRegion.png","last")
 
 for view in ViewRegion:
-
+	
+	
+	if view == "last":
+		for i in ViewRegion[0:3]:
+			DrawContour(i)
+			
+	
 	DrawContour(view)
+	
+	click("ContourSettings.png")
+	#change line width from 3 to 7
+	type(Key.UP+Key.UP+Key.UP+Key.UP+Key.UP)
+	click("cancel.png")
+	#check that nothing changed	
 
-	#click("Validate.png")
+
+	click("ContourSettings.png")
+	click("ok.png")
+	#check that line width changed.
+
+	click("ContourReinit.png")
+	#check that line disapears 
+	
+	DrawContour(view)
+	
+	if view == "last":
+		for i in ViewRegion[0:3]:
+			DrawContour(i)
+			
+	click("Validate.png")
+	#check that new contour is on View and in DB
+	
