1e041775338fd6d88c285bcd01bcd2874c0dc2ff
Evan Schwab <eschwab8@gmail.com>
2011-01-21 16:18:41 -0500

    Created MeshSegmentation test.  Draws 2D contours, 3D Spheres and 3D level sets on each view.
    NEED TO:  make loop for different drawing shapes.

---
+++ Testing/Sikuli/MeshSegmentation.sikuli/MeshSegmentation.py
@@ -0,0 +1,31 @@
+import os
+address = os.getcwd()
+#address = "/home/es1076/GITROOT/GoFigure2/Testing/Sikuli/gofigure2_images"
+
+#address = address+"/gofigure2_images"
+#print address
+setBundlePath(address)
+
+def MeshSegmentation(View)
+
+	import random
+	region = find(Pattern(View).similar(0.30))
+
+	#could make it a straight line by moving differentials in one direction.
+	xclick = random.randint(region.x,region.w+region.x)
+	yclick = random.randint(region.y,region.h+region.y)
+
+	click("Apply.png")
+
+
+
+ViewRegion = ("XY_ViewRegion.png","XZ_ViewRegion.png","YZ_ViewRegion.png","last")
+
+
+for view in ViewRegion:
+
+	if view == "last":
+		for i in ViewRegion[0:3]:
+			MeshSegmentation(i)
+	else:
+		MeshSegmentation(view)
