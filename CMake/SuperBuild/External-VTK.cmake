53184e6a318c94af8a096253018ee15b748eb169
Arnaud GELAS <arnaud_gelas@hms.harvard.edu>
2011-02-10 17:43:25 -0500

    Add linker flags on widnows to avoid confusion in between static and dynamic linked libraries

--- CMake/SuperBuild/External-VTK.cmake
+++ CMake/SuperBuild/External-VTK.cmake
@@ -4,6 +4,15 @@
 #-----------------------------------------------------------------------------
 set(proj VTK)
 
+if( WIN32 )
+	set( WINDOWS_FLAGS 
+	-DCMAKE_SHARED_LINKER_FLAGS:STRING=${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:libcmt
+	-DCMAKE_EXE_LINKER_FLAGS:STRING=${CMAKE_EXE_LINKER_FLAGS} /NODEFAULTLIB:libcmt 
+	)
+else( WIN32 )
+	set( WINDOWS_FLAGS )
+endif( WIN32 )
+
 ExternalProject_Add(${proj}
   SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
   BINARY_DIR ${proj}-build
@@ -22,6 +31,7 @@ ExternalProject_Add(${proj}
 	-DMYSQL_INCLUDE_DIRECTORIES:PATH=${MYSQL_INCLUDE_DIR}
 	-DMYSQL_LIBRARY:FILEPATH=${MYSQL_LIBRARIES}
     ${VIDEO_SUPPORT}
+	${WINDOWS_FLAGS}
   INSTALL_COMMAND ""
   DEPENDS
     ${VTK_DEPENDENCIES}
