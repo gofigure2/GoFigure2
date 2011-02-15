6605141d6a5676057406d924f3da2ba64fb8a3ea
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-02-15 15:48:45 -0500

    ENH: Makes FindSikuli.cmake works.
    Tested on Linux, Mac!
    Add one function to ease the process of adding a sikuli test with CTest

--- CMake/FindSikuli.cmake
+++ CMake/FindSikuli.cmake
@@ -1,29 +1,26 @@
 # - Find SIKULI
 
 IF( UNIX )
-  IF( NOT APPLE ) # Linux
-    FIND_PROGRAM( SH_EXECUTABLE
-      NAMES bash
+  FIND_PROGRAM( SH_EXECUTABLE
+    NAMES bash
     )
 
-    FIND_PROGRAM( SIKULI_EXECUTABLE
-      NAMES sikuli-ide.sh
-      PATHS
-      "$ENV{ProgramFiles}/Sikuli-IDE/"
-      "$ENV{SystemDrive}/Sikuli-IDE/"
-      "$ENV{HOME}/Sikuli-IDE/"
-      "$ENV{HOME}/Sikuli-IDE/Sikuli-IDE"
-      PATH_SUFFIXES "bin"
-      DOC "Specify the path to sikuli"
+  FIND_FILE( SIKULI_EXECUTABLE
+    NAMES sikuli-ide.sh
+    PATHS
+    "$ENV{ProgramFiles}/Sikuli-IDE/"
+    "$ENV{SystemDrive}/Sikuli-IDE/"
+    "$ENV{HOME}/Sikuli-IDE/"
+    "$ENV{HOME}/Sikuli-IDE/Sikuli-IDE"
+    "/Applications/Sikuli-IDE.app"
+    PATH_SUFFIXES "bin"
+    DOC "Specify the path to sikuli"
     )
-  ELSE( NOT APPLE ) # Mac OS
-    MESSAGE( "Apple" )
-  ENDIF( NOT APPLE )
 
 ELSE( UNIX ) # Windows
 
   FIND_PROGRAM( SIKULI_EXECUTABLE
-    NAMES sikuli
+    NAMES sikuli-ide.exe
     PATHS
      "$ENV{ProgramFiles}/Sikuli-IDE/"
      "$ENV{SystemDrive}/Sikuli-IDE/"
@@ -37,3 +34,13 @@ INCLUDE( FindPackageHandleStandardArgs )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Sikuli DEFAULT_MSG Sikuli_EXECUTABLE)
 
 MARK_AS_ADVANCED( SIKULI_EXECUTABLE )
+
+FUNCTION( add_sikuli_test testname sikuli_test )
+  IF( UNIX )
+    add_test( ${testname}
+      ${SH_EXECUTABLE} ${SIKULI_EXECUTABLE} -t ${sikuli_test} )
+  ELSE( UNIX )
+    add_test( ${testname}
+      ${SIKULI_EXECUTABLE} -t ${sikuli_test} )
+  ENDIF( UNIX )
+ENDFUNCTION( add_sikuli_test )
