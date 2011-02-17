845a8aa907a50327797d622e643bc6b7078c320d
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-02-17 13:30:39 -0500

    ENH: correct FFMPEG bugs - NOT WORKING

--- CMake/SuperBuild/External-FFMPEG.cmake
+++ CMake/SuperBuild/External-FFMPEG.cmake
@@ -17,16 +17,16 @@ ExternalProject_Add(${proj}
   
   # Configure step
   # DO STH FOR THE ARCHITECTURE...
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure
-    --prefix=<INSTALL_DIR> --enable-shared --enable-gpl --enable-avfilter --disable-yasm
+  CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/${proj}/configure
+   --enable-shared --enable-gpl --enable-avfilter --disable-yasm
 
   # Install the project
   INSTALL_COMMAND ""
 )
 
 set( VIDEO_SUPPORT -DVTK_USE_FFMPEG_ENCODER:BOOL=ON
-                   -DFFMPEG_INCLUDE_DIR=PATH:${CMAKE_BINARY_DIR}/${proj}
-                   -DFFMPEG_avcodec_LIBRARY=PATH:${CMAKE_BINARY_DIR}/${proj}/libavcodec/libavcodec.dylib            
-                   -DFFMPEG_avformat_LIBRARY=PATH:${CMAKE_BINARY_DIR}/${proj}/libavformat/libavformat.dylib           
-                   -DFFMPEG_avutil_LIBRARY=PATH:${CMAKE_BINARY_DIR}/${proj}/libavutil/lib/libavutil.dylib              
-                   -DFFMPEG_swscale_LIBRARY=PATH:${CMAKE_BINARY_DIR}/${proj}/libswscale/libswscale.dylib )
+                   -DFFMPEG_INCLUDE_DIR=STRING:${CMAKE_BINARY_DIR}/${proj}
+                   -DFFMPEG_avcodec_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libavcodec/libavcodec.dylib            
+                   -DFFMPEG_avformat_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libavformat/libavformat.dylib           
+                   -DFFMPEG_avutil_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libavutil/lib/libavutil.dylib              
+                   -DFFMPEG_swscale_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libswscale/libswscale.dylib )
