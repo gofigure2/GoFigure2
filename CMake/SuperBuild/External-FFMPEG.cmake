a96e1fbb00d0049a49bb8856763a5e9a11c382ac
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-02-17 17:33:13 -0500

    ENH: FFMPEG WORKING ON UBUNTU, SHARED AND STATIC
    TODO: TEST ON MAC

--- CMake/SuperBuild/External-FFMPEG.cmake
+++ CMake/SuperBuild/External-FFMPEG.cmake
@@ -5,6 +5,10 @@
 #-----------------------------------------------------------------------------
 set(proj FFMPEG)
 
+IF( SUPER_SHARED_LIBS )
+  set( SHARED_FFMPEG --enable-shared )
+ENDIF( SUPER_SHARED_LIBS )
+
 ExternalProject_Add(${proj}
   # Set up dirs
   SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
@@ -17,16 +21,34 @@ ExternalProject_Add(${proj}
   
   # Configure step
   # DO STH FOR THE ARCHITECTURE...
-  CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/${proj}/configure
-   --enable-shared --enable-gpl --enable-avfilter --disable-yasm
+  CONFIGURE_COMMAND <SOURCE_DIR>/configure ${SHARED_FFMPEG} --enable-gpl --enable-avfilter --disable-yasm
 
   # Install the project
   INSTALL_COMMAND ""
 )
 
+# REQUIRE FIND_LIBRARY TO GET THE PLATFORM SPECIFIC EXTENSIONS (so, a, dylib...)
+FIND_LIBRARY(FFMPEG_avformat_LIBRARY avformat
+  ${CMAKE_BINARY_DIR}/${proj}/libavformat
+)
+
+FIND_LIBRARY(FFMPEG_avcodec_LIBRARY avcodec
+  ${CMAKE_BINARY_DIR}/${proj}/libavcodec
+)
+
+FIND_LIBRARY(FFMPEG_avutil_LIBRARY avutil
+  ${CMAKE_BINARY_DIR}/${proj}/libavutil
+)
+
+FIND_LIBRARY(FFMPEG_swscale_LIBRARY swscale
+  ${CMAKE_BINARY_DIR}/${proj}/libswscale
+)
+
 set( VIDEO_SUPPORT -DVTK_USE_FFMPEG_ENCODER:BOOL=ON
-                   -DFFMPEG_INCLUDE_DIR=STRING:${CMAKE_BINARY_DIR}/${proj}
-                   -DFFMPEG_avcodec_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libavcodec/libavcodec.dylib            
-                   -DFFMPEG_avformat_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libavformat/libavformat.dylib           
-                   -DFFMPEG_avutil_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libavutil/lib/libavutil.dylib              
-                   -DFFMPEG_swscale_LIBRARY=STRING:${CMAKE_BINARY_DIR}/${proj}/libswscale/libswscale.dylib )
+                   -DVTK_FFMPEG_HAS_OLD_HEADER:BOOL=FALSE
+                   -DFFMPEG_INCLUDE_DIR:PATH=${CMAKE_BINARY_DIR}/${proj}
+                   -DFFMPEG_avcodec_LIBRARY:FILEPATH=${FFMPEG_avcodec_LIBRARY}          
+                   -DFFMPEG_avformat_LIBRARY:FILEPATH=${FFMPEG_avformat_LIBRARY}          
+                   -DFFMPEG_avutil_LIBRARY:FILEPATH=${FFMPEG_avutil_LIBRARY}            
+                   -DFFMPEG_swscale_LIBRARY:FILEPATH=${FFMPEG_swscale_LIBRARY}
+)
