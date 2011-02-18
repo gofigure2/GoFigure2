7346597fd82609e9511bdd722ed3585cd07ffe40
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-02-18 14:14:16 -0500

    ENH: force FFMPEG_NEW_HEADERS to TRUE in VTK/SuperFFMPEG

--- CMake/SuperBuild/External-FFMPEG.cmake
+++ CMake/SuperBuild/External-FFMPEG.cmake
@@ -40,11 +40,15 @@ ELSE( SUPER_SHARED_LIBS )
   SET(LIBRARY_SUFFIX .a)
 ENDIF( SUPER_SHARED_LIBS )
 
-set( VIDEO_SUPPORT -DVTK_FFMPEG_HAS_OLD_HEADER:BOOL=FALSE
+set( VIDEO_SUPPORT # turn ON ffmpeg encoder
+                   -DVTK_USE_FFMPEG_ENCODER:BOOL=ON
+                   # force VTK to use NEW headers
+                   -DVTK_FFMPEG_CACHED_INCLUDE:PATH=${CMAKE_BINARY_DIR}/${proj}
+                   -DVTK_FFMPEG_HAS_OLD_HEADER:BOOL=FALSE
+                   # set useful variables
                    -DFFMPEG_INCLUDE_DIR:PATH=${CMAKE_BINARY_DIR}/${proj}
                    -DFFMPEG_avcodec_LIBRARY:FILEPATH=${CMAKE_BINARY_DIR}/${proj}/libavcodec/libavcodec${LIBRARY_SUFFIX}         
                    -DFFMPEG_avformat_LIBRARY:FILEPATH=${CMAKE_BINARY_DIR}/${proj}/libavformat/libavformat${LIBRARY_SUFFIX}
                    -DFFMPEG_avutil_LIBRARY:FILEPATH=${CMAKE_BINARY_DIR}/${proj}/libavutil/libavutil${LIBRARY_SUFFIX}
                    -DFFMPEG_swscale_LIBRARY:FILEPATH=${CMAKE_BINARY_DIR}/${proj}/libswscale/libswscale${LIBRARY_SUFFIX}
-                   -DVTK_USE_FFMPEG_ENCODER:BOOL=ON
 )
