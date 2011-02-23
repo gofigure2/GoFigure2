9608cdd6db8f47340869f6fedf3f40d1afd88866
Evan Schwab <eschwab8@gmail.com>
2011-02-23 15:31:31 -0500

    TEMP

--- Testing/Sikuli/ToolbarInteractionModes.sikuli/ToolbarInteractionModes.py
+++ Testing/Sikuli/ToolbarInteractionModes.sikuli/ToolbarInteractionModes.py
@@ -1,12 +1,48 @@
-
+#will have to click trace and table to get right size for xyview images and navigation widget to see if things moved or not
 
 Toolbar = ("default","ZoomIcon.png","PanIcon.png","ActorPickingIcon.png")
 
+
+Mouse = ("Left","Right","Middle","Scroll")
+Actions = ("single_click","double_click","drag_up2down","drag_down2up","drag_left2right","drag_right2left")
+
+ViewRegion = ("XY_ViewRegion.png","XZ_ViewRegion.png","YZ_ViewRegion.png","XYZ_ViewRegion.png")
+
+for i in [0..3]:
+	Init_view[i] = find(Pattern(ViewRegion[i]).similar(0.30))
+	
+	#Init_XZview = find(Pattern(ViewRegion[1]).similar(0.30))
+	#Init_YZview = find(Pattern(ViewRegion[2]).similar(0.30))
+	#Init_XYZview = find(Pattern(ViewRegion[3]).similar(0.30))
+
+
 for mode in Toolbar:
 
-	 if mode != "default":
+	if mode != "default":
 	 	click(mode)
 
-	#button: left, right, middle, scroll
-	#action: single_click, double_click, drag_up2down,drag_down2up,drag_left2right,drag_right2left
-	#view: xy, xz, yz, xyz 	
+	for j in [0..3]:
+		point= [2*(Init_view[j].x+Init_view[j].h)/3, 2*(Init_view[j].y+Init_view[j].w)/3]
+		click(Location(point))
+
+
+		
+
+	
+
+	
+
+		
+
+	
+
+
+	
+
+		
+	
+	
+     	
+
+
+		
--- Testing/Sikuli/test.sikuli/test.py
+++ Testing/Sikuli/test.sikuli/test.py
@@ -1,19 +1,15 @@
+from sikuli.Sikuli import *
 
-#import sys
 
 #address = sys.argv[0]
-def test(self):
-	address = "/home/es1076/GITROOT/GoFigure2/Testing/Sikuli/googletest"
+def test():
+	
 
-	setBundlePath(address)
-	print address
+	
+
 
 	click("google.png")
-	#click("yo.png")
-	
-def test2(self):
-	address = "/home/es1076/GITROOT/GoFigure2/Testing/Sikuli/googletest"
 
-	setBundlePath(address)
-	print address
+	
+
 	click("1298417390867.png")
