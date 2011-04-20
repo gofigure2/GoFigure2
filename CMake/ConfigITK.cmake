37023883421fc2fa84f623803794d3c926d80fa0
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-04-20 15:23:20 -0400

    ENH: check if requiered modules are enabled

--- CMake/ConfigITK.cmake
+++ CMake/ConfigITK.cmake
@@ -13,11 +13,86 @@ FIND_PACKAGE( ITK REQUIRED )
     MESSAGE( SEND_ERROR "WRONG VERSION OF ITK, REQUIERES MODULARIZED ITK, CMAKE WILL STOP NOW")
   ENDIF ( NOT DEFINED ITK_MODULES_ENABLED )
 
-  # Check if "REVIEW" is "ON"
-  IF( ${ITK_USE_REVIEW} MATCHES "OFF" )
-    MESSAGE( SEND_ERROR
-      "GoFigure2 requires ITK to be compiled with Module_ITK-Review turned ON" )
-  ENDIF( ${ITK_USE_REVIEW} MATCHES "OFF" )
+  # check if requiered modules are enabled
+ list(FIND ITK_MODULES_ENABLED ITK-Common ITK-Common_FOUND)
+ IF( NOT ITK-Common_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-Common turned ON")
+ ENDIF( NOT ITK-Common_FOUND  )
+ 
+ list(FIND ITK_MODULES_ENABLED ITK-Review ITK-Review_FOUND)
+ IF( NOT ITK-Review_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-Review turned ON")
+ ENDIF( NOT ITK-Review_FOUND  )
+
+list(FIND ITK_MODULES_ENABLED ITK-IO-Base ITK-IO-Base_FOUND)
+ IF( NOT ITK-IO-Base_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-Base turned ON")
+ ENDIF( NOT ITK-IO-Base_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-JPEG ITK-IO-JPEG_FOUND)
+ IF( NOT ITK-IO-JPEG_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-JPEG turned ON")
+ ENDIF( NOT ITK-IO-JPEG_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-BMP ITK-IO-BMP_FOUND)
+ IF( NOT ITK-IO-BMP_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-BMP turned ON")
+ ENDIF( NOT ITK-IO-BMP_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-GDCM ITK-IO-GDCM_FOUND)
+ IF( NOT ITK-IO-GDCM_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-GDCM turned ON")
+ ENDIF( NOT ITK-IO-GDCM_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-LSM ITK-IO-LSM_FOUND)
+ IF( NOT ITK-IO-LSM_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-LSM turned ON")
+ ENDIF( NOT ITK-IO-LSM_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-PNG ITK-IO-PNG_FOUND)
+ IF( NOT ITK-IO-PNG_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-PNG turned ON")
+ ENDIF( NOT ITK-IO-PNG_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-TIFF ITK-IO-TIFF_FOUND)
+ IF( NOT ITK-IO-TIFF_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-TIFF turned ON")
+ ENDIF( NOT ITK-IO-TIFF_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-VTK ITK-IO-VTK_FOUND)
+ IF( NOT ITK-IO-VTK_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-VTK turned ON")
+ ENDIF( NOT ITK-IO-VTK_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-Stimulate ITK-IO-Stimulate_FOUND)
+ IF( NOT ITK-IO-Stimulate_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-Stimulate turned ON")
+ ENDIF( NOT ITK-IO-Stimulate_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-BioRad ITK-IO-BioRad_FOUND)
+ IF( NOT ITK-IO-BioRad_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-BioRad turned ON")
+ ENDIF( NOT ITK-IO-BioRad_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-Meta ITK-IO-Meta_FOUND)
+ IF( NOT ITK-IO-Meta_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-Meta turned ON")
+ ENDIF( NOT ITK-IO-Meta_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-NIFTI ITK-IO-NIFTI_FOUND)
+ IF( NOT ITK-IO-NIFTI_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-NIFTI turned ON")
+ ENDIF( NOT ITK-IO-NIFTI_FOUND  )
+
+ list(FIND ITK_MODULES_ENABLED ITK-IO-NRRD ITK-IO-NRRD_FOUND)
+ IF( NOT ITK-IO-NRRD_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-NRRD turned ON")
+ ENDIF( NOT ITK-IO-NRRD_FOUND  )
+ 
+ list(FIND ITK_MODULES_ENABLED ITK-IO-GIPL ITK-IO-GIPL_FOUND)
+ IF( NOT ITK-IO-GIPL_FOUND )
+   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-GIPL turned ON")
+ ENDIF( NOT ITK-IO-GIPL_FOUND  )
 
   set (ITK-IO  "ITK-IO-Base"
                "ITK-IO-JPEG"
