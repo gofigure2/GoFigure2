48705b66ca5a21db3dda5b5c7120e1bfff5df725
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-04-21 10:46:26 -0400

    Revert "ENH: superbuild working with the modularized itk (v4)"
    This reverts commit 5406c81bc906930e7ff761e99d6737a7a746c3b9.

--- CMake/SuperBuild/External-ITK.cmake
+++ CMake/SuperBuild/External-ITK.cmake
@@ -5,7 +5,7 @@ set(proj ITK)
 
 ExternalProject_Add(${proj}
   GIT_REPOSITORY "${git_protocol}://itk.org/ITK.git"
-  GIT_TAG "v4.0a07"
+  GIT_TAG "v4.0a06"
   UPDATE_COMMAND ""
   SOURCE_DIR ${proj}
   BINARY_DIR ${proj}-build
@@ -13,9 +13,7 @@ ExternalProject_Add(${proj}
   CMAKE_ARGS
     ${ep_common_args}
     -DITK_INSTALL_LIB_DIR:PATH=${GOFIGURE2_INSTALL_LIB_DIR}
-    #to be modified
-    -DITK_BUILD_ALL_MODULES:BOOL=ON
-    -DModule_ITK-Review:INTERNAL=ON
+    -DITK_USE_REVIEW:BOOL=ON
   INSTALL_COMMAND ""
   DEPENDS
     ${ITK_DEPENDENCIES}
