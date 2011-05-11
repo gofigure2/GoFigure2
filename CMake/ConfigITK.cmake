5805f5fc47160c407ae71979143f18f59e2266d5
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-05-11 15:30:24 -0400

    COMP: builds with itkv4 & gcc 4.6

--- CMake/ConfigITK.cmake
+++ CMake/ConfigITK.cmake
@@ -13,109 +13,12 @@ FIND_PACKAGE( ITK REQUIRED )
     MESSAGE( SEND_ERROR "WRONG VERSION OF ITK, REQUIERES MODULARIZED ITK, CMAKE WILL STOP NOW")
   ENDIF ( NOT DEFINED ITK_MODULES_ENABLED )
 
-MESSAGE(STATUS "${ITK_MODULES_ENABLED}")
-
   # check if requiered modules are enabled
- list(FIND ITK_MODULES_ENABLED ITK-Common ITK-Common_FOUND)
- IF( ITK-Common_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-Common turned ON")
- ENDIF( ITK-Common_FOUND LESS 0  )
-
- list(FIND ITK_MODULES_ENABLED ITK-Review ITK-Review_FOUND)
- IF( ITK-Review_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-Review turned ON")
- ENDIF( ITK-Review_FOUND LESS 0 )
-
-list(FIND ITK_MODULES_ENABLED ITK-IO-Base ITK-IO-Base_FOUND)
- IF( ITK-IO-Base_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-Base turned ON")
- ENDIF( ITK-IO-Base_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-JPEG ITK-IO-JPEG_FOUND)
- IF( ITK-IO-JPEG_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-JPEG turned ON")
- ENDIF( ITK-IO-JPEG_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-BMP ITK-IO-BMP_FOUND)
- IF( ITK-IO-BMP_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-BMP turned ON")
- ENDIF( ITK-IO-BMP_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-GDCM ITK-IO-GDCM_FOUND)
- IF( ITK-IO-GDCM_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-GDCM turned ON")
- ENDIF( ITK-IO-GDCM_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-LSM ITK-IO-LSM_FOUND)
- IF( ITK-IO-LSM_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-LSM turned ON")
- ENDIF( ITK-IO-LSM_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-PNG ITK-IO-PNG_FOUND)
- IF( ITK-IO-PNG_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-PNG turned ON")
- ENDIF( ITK-IO-PNG_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-TIFF ITK-IO-TIFF_FOUND)
- IF( ITK-IO-TIFF_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-TIFF turned ON")
- ENDIF( ITK-IO-TIFF_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-VTK ITK-IO-VTK_FOUND)
- IF( ITK-IO-VTK_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-VTK turned ON")
- ENDIF( ITK-IO-VTK_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-Stimulate ITK-IO-Stimulate_FOUND)
- IF( ITK-IO-Stimulate_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-Stimulate turned ON")
- ENDIF( ITK-IO-Stimulate_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-BioRad ITK-IO-BioRad_FOUND)
- IF( ITK-IO-BioRad_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-BioRad turned ON")
- ENDIF( ITK-IO-BioRad_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-Meta ITK-IO-Meta_FOUND)
- IF( ITK-IO-Meta_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-Meta turned ON")
- ENDIF( ITK-IO-Meta_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-NIFTI ITK-IO-NIFTI_FOUND)
- IF( ITK-IO-NIFTI_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-NIFTI turned ON")
- ENDIF( ITK-IO-NIFTI_FOUND LESS 0 )
-
- list(FIND ITK_MODULES_ENABLED ITK-IO-NRRD ITK-IO-NRRD_FOUND)
- IF( ITK-IO-NRRD_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-NRRD turned ON")
- ENDIF(ITK-IO-NRRD_FOUND LESS 0 )
- 
- list(FIND ITK_MODULES_ENABLED ITK-IO-GIPL ITK-IO-GIPL_FOUND)
- IF(ITK-IO-GIPL_FOUND LESS 0 )
-   MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-IO-GIPL turned ON")
- ENDIF( ITK-IO-GIPL_FOUND LESS 0 )
-
-  set (ITK-IO  "ITK-IO-Base"
-               "ITK-IO-JPEG"
-               "ITK-IO-BMP"
-               "ITK-IO-GDCM"
-	       "ITK-IO-LSM"
-	       "ITK-IO-PNG"
-	       "ITK-IO-TIFF"
-	       "ITK-IO-VTK"
-	       "ITK-IO-Stimulate"
-	       "ITK-IO-BioRad"
-	       "ITK-IO-Meta"
-	       "ITK-IO-NIFTI"
-	       "ITK-IO-NRRD"
-	       "ITK-IO-GIPL"
-               # MRC is in review....
-               #"ITK-IO-MRC"
-               # test IO review
-               "ITK-Review"
-               
-)
+  # note that ITK-Review requires all modules to be turned ON...
+  list(FIND ITK_MODULES_ENABLED ITK-Review ITK-Review_FOUND)
+  IF( ITK-Review_FOUND LESS 0 )
+    MESSAGE( SEND_ERROR "GoFigure2 requires ITK to be compiled with ITK-Review turned ON")
+  ENDIF( ITK-Review_FOUND LESS 0 )
 
 ELSE( ITK_FOUND )
     MESSAGE( SEND_ERROR "ITK NOT FOUND, CMAKE WILL STOP NOW")
--- CMake/SuperBuild/External-ITK.cmake
+++ CMake/SuperBuild/External-ITK.cmake
@@ -3,22 +3,6 @@
 
 set(proj ITK)
 
-set (ITK-IO  "-DITK-IO-Base:INTERNAL=ON"
-             "-DITK-IO-JPEG:INTERNAL=ON"
-             "-DITK-IO-BMP:INTERNAL=ON"
-             "-DITK-IO-GDCM:INTERNAL=ON"
-             "-DITK-IO-LSM:INTERNAL=ON"
-             "-DITK-IO-PNG:INTERNAL=ON"
-             "-DITK-IO-TIFF:INTERNAL=ON"
-             "-DITK-IO-VTK:INTERNAL=ON"
-             "-DITK-IO-Stimulate:INTERNAL=ON"
-             "-DITK-IO-BioRad:INTERNAL=ON"
-             "-DITK-IO-Meta:INTERNAL=ON"
-             "-DITK-IO-NIFTI:INTERNAL=ON"
-             "-DITK-IO-NRRD:INTERNAL=ON"
-             "-DITK-IO-GIPL:INTERNAL=ON"
-           )
-
 ExternalProject_Add(${proj}
   GIT_REPOSITORY "${git_protocol}://itk.org/ITK.git"
   GIT_TAG "v4.0a07"
@@ -29,11 +13,8 @@ ExternalProject_Add(${proj}
   CMAKE_ARGS
     ${ep_common_args}
     -DITK_INSTALL_LIB_DIR:PATH=${GOFIGURE2_INSTALL_LIB_DIR}
-    -DModule_ITK-Common:INTERNAL=ON
-    -DModule_ITK-Statistics:INTERNAL=ON
      # should change soon
     -DITK_USE_REVIEW:INTERNAL=ON
-    ${ITK-IO}
   INSTALL_COMMAND ""
   DEPENDS
     ${ITK_DEPENDENCIES}
